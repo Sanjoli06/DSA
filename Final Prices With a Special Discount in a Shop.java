1c5ce8617f522db6dfa7988b2d95939a09af0c98
Sanjoli06 <goyalsanjoli036@gmail.com>
2026-02-16 23:36:18 +0530

    Day 15

---
+++ Final Prices With a Special Discount in a Shop.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        Stack<Integer> st=new Stack();
+        int[] ans=new int[prices.length];
+        int n=prices.length;
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && prices[i]<prices[st.peek()]){
+                st.pop();
+            }
+            if(!st.isEmpty()){
+                ans[i]=prices[i]-prices[st.peek()];
+            }
+            else{
+                ans[i]=prices[i];
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
