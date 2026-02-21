10dde15015de58fc246993a465a75c9619d225e3
Sanjoli06 <goyalsanjoli036@gmail.com>
2026-02-21 23:12:31 +0530

    Day 20

---
+++ Integer to Roman.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder ans=new StringBuilder();
+        int l=0;
+        while(num!=0){
+            while(values[l]>num){
+                l++;
+            }
+            ans.append(symbols[l]);
+            num-=values[l];
+        }
+        return ans.toString();
+    }
+}
