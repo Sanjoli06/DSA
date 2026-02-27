288516b0b59d8ad6bd3fe7570e693509b15891a0
SanjoliGoyal <139862315+Sanjoli06@users.noreply.github.com>
2026-02-27 11:55:00 +0530

    Day 26

---
+++ TwoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans=new int[2];
+        HashMap<Integer,Integer> map=new HashMap();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                int fi=map.get(target-nums[i]);
+                ans[0]=fi;
+                ans[1]=i;
+                break;
+            }
+            map.put(nums[i],i);
+        }
+        return ans;
+    }
+}g
